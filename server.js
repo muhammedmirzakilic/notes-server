0b9aab83a56d43a1648a9da6f54440feb3add18c
Mirza <muhammedmirzakilic@gmail.com>
2020-06-07 01:21:51 +0300

    crud operations implemented

--- server.js
+++ server.js
@@ -16,20 +16,84 @@ router
     context.response.body = "Hello world!";
   })
   .get("/notes/getall", async (context) => {
-    //TODO: return all notes
+    let notes = await notesCollection.find({ isDeleted: { $ne: true } });
+    notes = notes.map((note) => {
+      var { _id: {$oid: id}, title, description, creadetAt, updatedAt } = note;
+      return {
+        id,
+        title,
+        description,
+        creadetAt,
+        updatedAt,
+      };
+    });
     context.response.body = notes;
   })
-  .get("/notes/:id", (context) => {
-    //TODO: return spesified note
+  .get("/notes/:id", async (context) => {
+    if (!context.params || !context.params.id) {
+      return context.response.body = {};
+    }
+    var note = await notesCollection.findOne(
+      { _id: { "$oid": context.params.id } },
+    );
+    var { _id: {$oid: id}, title, description, creadetAt, updatedAt } = note;
+    context.response.body = {
+      id,
+      title,
+      description,
+      creadetAt,
+      updatedAt,
+    };
   })
-  .post("/notes/insert", (context) => {
-    //TODO: insert note
+  .post("/notes/insert", async (context) => {
+    if (!context.request.hasBody) {
+      return context.response.status = 400;
+    }
+    const body = await context.request.body();
+    var { title, description } = body.value;
+    if (!title || !description) {
+      return context.response.status = 400;
+    }
+    const result = await notesCollection.insertOne({
+      title,
+      description,
+      createdAt: new Date(),
+    });
+    context.response.body = { result };
   })
-  .post("/notes/update", (context) => {
-    //TODO: update note
+  .post("/notes/update", async (context) => {
+    if (!context.request.hasBody) {
+      return context.response.status = 400;
+    }
+    const body = await context.request.body();
+    var { title, description, id } = body.value;
+    if (!id || !title || !description) {
+      return context.response.status = 400;
+    }
+    const { modifiedCount } = await notesCollection.updateOne(
+      { _id: { "$oid": id } },
+      {
+        $set: { title, description, updatedAt: new Date() },
+      },
+    );
+    context.response.body = { result: modifiedCount == 1 };
   })
-  .post("/notes/delete", (context) => {
-    //TODO: delete note
+  .post("/notes/delete", async (context) => {
+    if (!context.request.hasBody) {
+      return context.response.status = 400;
+    }
+    const body = await context.request.body();
+    var { title, description, id } = body.value;
+    if (!id || !title || !description) {
+      return context.response.status = 400;
+    }
+    const { modifiedCount } = await notesCollection.updateOne(
+      { _id: { "$oid": id } },
+      {
+        $set: { isDeleted: true },
+      },
+    );
+    context.response.body = { result: modifiedCount == 1 };
   });
 
 const app = new Application();
